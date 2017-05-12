e6e525347d6f6c96aea18849bdbd8a3815111043
Richard Abbenhuis <richard@abbenhuis.net>
2017-05-12 14:06:38 +0200

    Add is-supported function

--- bash/profile.d/function.sh
+++ bash/profile.d/function.sh
@@ -23,3 +23,19 @@ function get_xserver() {
 			;;
 	esac
 }
+
+is-supported() {
+	if [[ $# -eq 1 ]] ; then
+		if eval "$1" > /dev/null 2>&1 ; then
+			true;
+		else
+			false;
+		fi
+	else
+		if eval "$1" > /dev/null 2>&1 ; then
+			echo -n "$2"
+		else
+			echo -n "$3"
+		fi
+	fi
+}
